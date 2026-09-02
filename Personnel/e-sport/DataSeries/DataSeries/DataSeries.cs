00643ba9fb76f41c51416e5f022a2926fefee839
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-09-02 10:44:40 +0200

    feat(timeSeries): ajout des time series
    [15][DONE]

--- Personnel/e-sport/DataSeries/DataSeries/DataSeries.cs
+++ Personnel/e-sport/DataSeries/DataSeries/DataSeries.cs
@@ -1,28 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataSeries
 {
     public class DataSeries<T>
     {
-        private readonly IEnumerable<T> _data;
+        private readonly IEnumerable<DataPoint<T>> _data;
 
-        private DataSeries(IEnumerable<T> data) => _data = data;
+        private DataSeries(IEnumerable<DataPoint<T>> data) => _data = data;
 
-        public static DataSeries<T> From(IEnumerable<T> source) => new DataSeries<T>(source);
+        // Permet de créer une DataSeries à partir d'une liste de T (en créant un DataPoint par défaut)
+        public static DataSeries<T> From(IEnumerable<T> source)
+        {
+            var points = source.Select(item => new DataPoint<T>(DateTime.Now, item));
+            return new DataSeries<T>(points);
+        }
 
+        // Permet de créer une DataSeries à partir d'une liste de DataPoint<T>
+        public static DataSeries<T> From(IEnumerable<DataPoint<T>> source) => new DataSeries<T>(source);
+
+        public IEnumerable<T> Values => _data.Select(dp => dp.Value);
+        public IEnumerable<DataPoint<T>> DataPoints => _data;
         public int Count => _data.Count();
-        public IEnumerable<T> Values => _data;
 
         public static DataSeries<T> FromCsv(string path, Func<string[], T> parser)
         {
-            {
-                var lines = File.ReadAllLines(path).Skip(1); // ignorer l'en-tête
-                return new DataSeries<T>(lines.Select(line => parser(line.Split(','))));
-            }
+            var lines = File.ReadAllLines(path).Skip(1); // ignorer l'en-tête
+            var items = lines.Select(line => new DataPoint<T>(DateTime.Now, parser(line.Split(','))));
+            return new DataSeries<T>(items);
         }
     }
 }
