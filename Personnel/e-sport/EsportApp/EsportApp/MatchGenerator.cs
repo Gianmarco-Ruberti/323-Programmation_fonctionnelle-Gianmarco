4bbcf1b29a63f0bf36e840526738de88ad30ab20
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-09-16 10:37:48 +0200

    fix(DataPoint): suppression du dataPoint dans un fichier oublier

--- Personnel/e-sport/EsportApp/EsportApp/MatchGenerator.cs
+++ Personnel/e-sport/EsportApp/EsportApp/MatchGenerator.cs
@@ -9,18 +9,19 @@ namespace EsportApp
 {
     public static class MatchGenerator
     {
-        public static DataSeries<DataPoint<Cs2Match>> GenerateCs2(string player, int count, int seed = 42)
+        public static DataSeries<Cs2Match> GenerateCs2(string player, int count, int seed = 42)
         {
             var rng = new Random(seed);
             var maps = new[] { "Dust2", "Mirage", "Inferno", "Nuke", "Ancient" };
             var sides = new[] { "CT", "T" };
             var start = new DateTime(2023, 9, 1); // début de la pré-saison
 
-            return DataSeries<DataPoint<Cs2Match>>.From(
+            return DataSeries<Cs2Match>.From(
                 Enumerable.Range(1, count)
-                    .Select(i => new DataPoint<Cs2Match>(
-                        start.AddDays(i),
-                        new Cs2Match(player, maps[rng.Next(maps.Length)],
+                    .Select(i => new Cs2Match(
+                        start.AddDays(i).ToString("yyyy-MM-dd"),
+                        player, 
+                        maps[rng.Next(maps.Length)],
                         sides[rng.Next(2)],
                         rng.Next(10, 28),   // kills
                         rng.Next(6, 18),    // deaths
@@ -28,7 +29,7 @@ namespace EsportApp
                         rng.Next(0, 5),     // mvps
                         rng.Next(2) == 0    // won)
                         )
-                    ))
+                    )
             );
         }
     }
